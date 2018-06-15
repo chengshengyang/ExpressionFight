838d230b6ec2b60f368fce7456b4ea5ad9b2721a
chengshengyang <csyzyl2008.11.11@163.com>
2018-06-15 16:33:38 +0800

    配置各个分享平台的appId和appKey

--- app/src/main/java/com/csy/fight/preview/PreviewActivity.java
+++ app/src/main/java/com/csy/fight/preview/PreviewActivity.java
@@ -139,11 +139,9 @@ public class PreviewActivity extends AppCompatActivity {
         // text是分享文本，所有平台都需要这个字段
         //oks.setText("我是分享文本");
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        oks.setImagePath(mAlbumInfo.getPhotoList().get(mCurrentPosition).getImagePath());//确保SDcard下面存在此张图片
+        oks.setImagePath(mAlbumInfo.getPhotoList().get(mCurrentPosition).getImagePath());
         // url在微信、微博，Facebook等平台中使用
-        oks.setUrl("http://sharesdk.cn");
-        // comment是我对这条分享的评论，仅在人人网使用
-        //oks.setComment("我是测试评论文本");
+        // oks.setUrl("http://sharesdk.cn");
         // 启动分享GUI
         oks.show(this);
     }
